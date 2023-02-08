4b7455e1058ea4a09ca1fc2f318ddf88918bb31f
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2023-02-08 15:07:35 +0100

    [Update] Project UI

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+import drawLine from "/bibliotheques/svgPathHelper.js"
 
-import
-
-const svgContainer = document.querySelector(".svg-container")
+const svgContainer = document.querySelector("svg")
 
 svgContainer.innerHTML = drawRectangle()
 
 function drawRectangle() {
+    const rectSize = 300
 
+    return `${drawLine(0,0,rectSize,0)} 
+     ${drawLine(rectSize,0,rectSize,rectSize)} 
+     ${drawLine(0,0,0,rectSize)} 
+     ${drawLine(0,rectSize,rectSize,rectSize)} `
 }
 
 
